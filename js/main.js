4328dcd607afa6b7cf044028dc241ffadf4527ec
Mario <gserna.ms@gmail.com>
2023-06-05 22:39:08 +0200

    update home

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 function mostrar() {
-  document.getElementsByClassName("playerone").style.display = "block";
+  document.getElementById("bando").style.display = "block";
+  document.getElementById("empezar").style.display = "none";
 }
 
 function playAudio() {
   document.getElementById("myaudio").play();
+}
+
+function showPlayerOne() {
+  document.getElementById("playerone").style.display = "block";
+  document.getElementById("bando").style.display = "none";
 }
